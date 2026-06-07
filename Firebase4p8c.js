bacd994b6d34dd338fefa6956ed1bf1138a5ff07
nineloonking <heainf@gmail.com>
2026-06-07 13:12:35 +0800

    Create Firebase4p8c.js

---
+++ Firebase4p8c.js
@@ -0,0 +1,13 @@
+<script>
+// Firebase Config（部署時會被 GitHub Actions 自動替換）
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY_HERE",   // ← 這行會被自動替換
+  authDomain: "p8c-d1673.firebaseapp.com",
+  projectId: "p8c-d1673",
+  storageBucket: "p8c-d1673.firebasestorage.app",
+  messagingSenderId: "352251453242",
+  appId: "1:352251453242:web:37cfd6e5bd12f05c10fa03"
+};
+
+window.firebaseConfig = firebaseConfig;
+</script>
